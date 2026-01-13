d3575deaa4caa22d9b19e36278b0590ed44f51e3
James <jejedi110@pm.me>
2026-01-13 14:50:34 -0700

    Stacks

--- sandbox/sandbox_project/Program.cs
+++ sandbox/sandbox_project/Program.cs
@@ -4,34 +4,6 @@ public class Program
 {
     static void Main(string[] args)
     {
-        // This project is here for you to use as a "Sandbox" to play around
-        // with any code or ideas you have that do not directly apply to
-        // one of your projects.
 
-        Console.WriteLine("Hello Sandbox World!");
-        List<int> data = new() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        int amount = 1;
-
-        int index = 0;
-        List<int> rotatedData = new List<int>();
-        foreach (int item in data)
-        {
-            rotatedData.Add(0);
-        }
-        foreach (int item in data)
-        {
-            int newIndex = (index + amount) % data.Count;
-            Console.WriteLine(newIndex);
-            rotatedData[newIndex] = item;
-            index++;
-        }
-            Console.WriteLine("Final");
-        data = rotatedData;
-        foreach (int item in data)
-        {
-        Console.WriteLine(item);
-        }
-        
-        Console.WriteLine(data[0]);
     }
 }
